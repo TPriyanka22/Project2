c9248211f4ff160c5507659d9bd519407da75869
TPriyanka22 <priyanka.turumalla@gmail.com>
2022-11-14 13:30:11 -0800

    Adding the chatbot in userview

--- src/MovieBooking/UserView/UserView.jsx
+++ src/MovieBooking/UserView/UserView.jsx
@@ -82,7 +82,28 @@ class UserView extends PureComponent {
 
                             </Row>
                         </Col>
-                        
+                        <Col xl={4} >
+                
+                            <Row style={{ border: "1px solid black", margin: 10, borderRadius: 5 }}>
+                                <Card style={{ width: "100%", height: (window.innerHeight/2)+62  }}>
+                                    {/* <Alert variant="primary" style={{ margin: 10 }}>
+                                        <Alert.Heading>ChatBot</Alert.Heading>
+                                    </Alert> */}
+                                    {/* <Card.Header as="h4">ChatBot</Card.Header> */}
+                            <Card.Body >
+                                        <ChatBot
+                                            title="Movie Booking Chat Bot"
+                                            botName= "BookTrip_dev"
+                                            welcomeMessage="Welcome, how can I help you today?"
+                                            onComplete={this.handleComplete.bind(this)}
+                                            voiceEnabled={true}
+                                            clearOnComplete={true}
+                                        />
+                                    </Card.Body>
+                                </Card>
+                            </Row>
+
+                        </Col>
                     </Row>
                 </Container>
             </div>
