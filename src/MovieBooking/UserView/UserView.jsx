af691a6737752f4173185269977df2276d200052
TPriyanka22 <priyanka.turumalla@gmail.com>
2022-11-16 18:45:26 -0800

    Css updates to user view

--- src/MovieBooking/UserView/UserView.jsx
+++ src/MovieBooking/UserView/UserView.jsx
@@ -63,44 +63,50 @@ class UserView extends PureComponent {
                 <Container fluid>
                    
                     <Row style={{ marginTop: 10 }}>
-                        <Col xl={7} style={{ border: "1px solid black", margin: 10, borderRadius: 5, marginLeft: 80, height: (window.innerHeight) - 205, overflowY: "scroll" }}>
+                        <Col xl={7} style={{ margin: 10, borderRadius: 5, marginLeft: 80, height: (window.innerHeight) - 150, overflowY: "scroll" }}>
 
-                            <Alert variant="primary" style={{ marginTop: 10 }}>
+                            {/* <Alert variant="primary" style={{ marginTop: 10 }}>
                                 <Alert.Heading>Movies</Alert.Heading>
-                            </Alert>
+                            </Alert> */}
 
-                            <Row style={{ marginLeft: 35 }}>
+                            {/* <Row>
 
+                               
+
+                            </Row> */}
+                            <div className='card-container user-view text-center'>
                                 {searchData && searchData.map(movieData => {
-                                    return (
-                                        <Col>
-                                            <Movies movieData={movieData} key={movieData.movie_id}></Movies>
-                                        </Col>
-                                    )
-                                })
+                                        return (
+                                            // <Col>
+                                                <Movies movieData={movieData} key={movieData.movie_id}></Movies>
+                                            // </Col>
+                                        )
+                                    })
                                 }
-
-                            </Row>
+                            </div>
                         </Col>
                         <Col xl={4} >
                 
-                            <Row style={{ border: "1px solid black", margin: 10, borderRadius: 5 }}>
-                                <Card style={{ width: "100%", height: (window.innerHeight/2)+62  }}>
-                                    {/* <Alert variant="primary" style={{ margin: 10 }}>
+                            <Row>
+                                <Container style={{marginTop: "10px"}}>
+                                    <ChatBot
+                                        title="Movie Booking Chat Bot"
+                                        botName= "BookTrip_dev"
+                                        welcomeMessage="Welcome, how can I help you today?"
+                                        onComplete={this.handleComplete.bind(this)}
+                                        voiceEnabled={true}
+                                        clearOnComplete={true}
+                                    />
+                                </Container>
+                                {/* <Card style={{ width: "100%", height: (window.innerHeight/2)+62  }}>
+                                    <Alert variant="primary" style={{ margin: 10 }}>
                                         <Alert.Heading>ChatBot</Alert.Heading>
-                                    </Alert> */}
-                                    {/* <Card.Header as="h4">ChatBot</Card.Header> */}
-                            <Card.Body >
-                                        <ChatBot
-                                            title="Movie Booking Chat Bot"
-                                            botName= "BookTrip_dev"
-                                            welcomeMessage="Welcome, how can I help you today?"
-                                            onComplete={this.handleComplete.bind(this)}
-                                            voiceEnabled={true}
-                                            clearOnComplete={true}
-                                        />
+                                    </Alert>
+                                    {<Card.Header as="h4">ChatBot</Card.Header>}
+                                    <Card.Body >
+                                        
                                     </Card.Body>
-                                </Card>
+                                </Card> */}
                             </Row>
 
                         </Col>
