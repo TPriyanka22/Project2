419edbc97446d95bea8c7bb95e441278834e4a95
Sri Akhil Thaneti <akhilthaneti24@gmail.com>
2022-11-17 04:28:47 -0800

    Update Home.js

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,31 +1,20 @@
-import React, { Component } from "react";
 import { Row, Col, Alert } from "react-bootstrap";
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
 import "./Home.css"
+import { Link } from 'react-router-dom';
+
 class Home extends Component {
+  
   
   componentDidMount(){
     sessionStorage.setItem("userEmail", "")
   }
   render() {
     return (
-
-      // <div>
-      //   <Col xl={{ span: 6, offset: 3 }}>
-      //     <Row style={{ display: "block" }}>
-      //       <Alert variant="success" style={{ marginTop: "200px" }} className="text-center">
-      //         <Alert.Heading>Welcome to ShowTime Drivein</Alert.Heading>
-      //       </Alert>
-      //     </Row>
-      //   </Col>
-      
-      // </div>
-     
       <div class="wrapper">
       <div class="neon-wrapper">
           <div class="txt">
           SHOWTIME DRIVEIN
-
           </div>
           <span class="gradient"></span>
           <span class="dodge"></span>
