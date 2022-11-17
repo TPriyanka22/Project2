f48e989eb6d1c9d706116ad28f8ac0400919726b
Sri Akhil Thaneti <akhilthaneti24@gmail.com>
2022-11-17 03:59:41 -0800

    Update Routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { Switch } from "react-router-dom";
-import Home from "./containers/Home";
-import CreateMovie from "./MovieBooking/AdminView/CreateMovie"
-import AppliedRoute from "./containers/AppliedRoute";
 import UserView from "./MovieBooking/UserView/UserView";
-import AdminView from "./MovieBooking/AdminView/AdminView";
 import RedirectPage from "./MovieBooking/RedirectPage";
-import UpdateMovie from "./MovieBooking/AdminView/UpdateMovie";
 import RenderMovie from "./MovieBooking/UserView/RenderMovie";
 import UserBookings from "./MovieBooking/UserView/UserBookings";
-
+import { Switch } from "react-router-dom";
+import Home from "./containers/Home";
+import AdminView from "./MovieBooking/AdminView/AdminView";
+import CreateMovie from "./MovieBooking/AdminView/CreateMovie"
+import UpdateMovie from "./MovieBooking/AdminView/UpdateMovie";
+import AppliedRoute from "./containers/AppliedRoute";
 
 export default ({ childProps }) =>
   <Switch>
+  
     <AppliedRoute path="/" exact component={Home} props={childProps} />
     <AppliedRoute path="/CreateMovie" exact component={CreateMovie} props={childProps} />
     <AppliedRoute path="/Redirect" exact component={RedirectPage} props={childProps} />   
@@ -22,4 +22,5 @@ export default ({ childProps }) =>
     <AppliedRoute path="/BookEvent/:id" exact component={RenderMovie} props={childProps} />
     <AppliedRoute path="/UserBookings" exact component={UserBookings} props={childProps} />
     
+      
   </Switch>;
