2fcc830f87f21dea91911f6586f9051dcc7cd51f
Sri Akhil Thaneti <akhilthaneti24@gmail.com>
2022-11-17 03:40:55 -0800

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router } from "react-router-dom";
+import config from './aws-exports' // new
+import Amplify from 'aws-amplify' // new
+Amplify.configure(config) // new
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root')
-);
+  
+    <Router>
+      <App />
+      </Router>,
+    document.getElementById("root")
+  );
