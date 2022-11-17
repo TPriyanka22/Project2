c1b2abff3ce6a256707efa91a89bf57699b944d1
saisankethravva <saisanketh621@gmail.com>
2022-11-17 04:11:30 -0800

    added amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,15 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "finalcloudprojectrea": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string"
+        }
+    },
+    "interactions": {
+        "lex8ed698b5": {
+            "Region": "string",
+            "BotName": "string",
+            "FunctionArn": "string"
+        }
+    }
+}
